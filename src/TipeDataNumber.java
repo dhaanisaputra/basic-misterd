848eb1c3d3bb7e416ce21aebcb49a5cfce0a8fee
HIWIGO <hiwigo@pop-os.localdomain>
2021-01-23 20:35:05 +0700

    first commit

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,9 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 1000000;
+        long iniLong = 1000000000;
+        long iniLong2 = 100000000000L; //opsi lain penulisan tipe long
+    }
+}
